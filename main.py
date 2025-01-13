54190657bd387c842090490b33248e4a95b4b196
519807 <519807@student.ue.wroc.pl>
2025-01-13 17:36:04 +0100

    Dodałam do formularza pola do wypełnienia odnośnie miasta, kraju i daty urodzenia.

--- main.py
+++ main.py
@@ -26,13 +26,28 @@ label_PhoneNr.grid(row=3, column=0, padx=10, pady=5, sticky="w")
 entry_PhoneNr = tk.Entry(root, width=40)
 entry_PhoneNr.grid(row=3, column=1, padx=10, pady=5)
 
+label_City = tk.Label(root, text="Miasto:")
+label_City.grid(row=4, column=0, padx=10, pady=5, sticky="w")
+entry_City = tk.Entry(root, width=40)
+entry_City.grid(row=4, column=1, padx=10, pady=5)
+
+label_Country = tk.Label(root, text="Kraj:")
+label_Country.grid(row=5, column=0, padx=10, pady=5, sticky="w")
+entry_Country = tk.Entry(root, width=40)
+entry_Country.grid(row=5, column=1, padx=10, pady=5)
+
+label_Data_Birth = tk.Label(root, text="Data Urodzenia:")
+label_Data_Birth.grid(row=6, column=0, padx=10, pady=5, sticky="w")
+entry_Data_Birth = tk.Entry(root, width=40)
+entry_Data_Birth.grid(row=6, column=1, padx=10, pady=5)
+
 # Dodanie kolejny Etykiet oraz pola tekstowego przez pozostałych członków grupy
 
 
 
 # Przycisk zapisu
 button_save = tk.Button(root, text="Zapisz", command=lambda x:x)
-button_save.grid(row=4, column=0, columnspan=2, pady=10)
+button_save.grid(row=7, column=0, columnspan=2, pady=10)
 
 # Uruchomienie pętli głównej
 root.mainloop()
